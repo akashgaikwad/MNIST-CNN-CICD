19b8cede641cea78e23c50f96a49f661c97cb020
akash gaikwad <akash.r.gaikwad@gmail.com>
2024-12-14 00:29:51 +0530

    Added batch normalization

--- src/model.py
+++ src/model.py
@@ -6,10 +6,13 @@ class MNISTNet(nn.Module):
         super(MNISTNet, self).__init__()
         # First conv layer: 1->16
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
         # Second conv layer: 16->32
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
         # Third conv layer: 32->32
         self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(32)
         # Fully connected layer: 32*3*3->10
         self.fc = nn.Linear(32 * 3 * 3, 10)
         
@@ -18,9 +21,9 @@ class MNISTNet(nn.Module):
 
     def forward(self, x):
         # 28x28 -> 14x14 -> 7x7 -> 3x3
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = self.pool(self.relu(self.conv3(x)))
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))
+        x = self.pool(self.relu(self.bn3(self.conv3(x))))
         x = x.view(-1, 32 * 3 * 3)
         x = self.fc(x)
         return x
