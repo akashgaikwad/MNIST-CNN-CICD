94a03f8210ba98bac14d9cb88495e8c524537b25
akash gaikwad <akash.r.gaikwad@gmail.com>
2024-12-14 12:15:44 +0530

    removed normalization, reduced batch size, increased learning rate

--- src/train.py
+++ src/train.py
@@ -11,8 +11,8 @@ def train(save_model=True):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     # Hyperparameters
-    batch_size = 64
-    learning_rate = 0.001
+    batch_size = 16
+    learning_rate = 0.003
     
     # MNIST Dataset
     transform = transforms.Compose([
